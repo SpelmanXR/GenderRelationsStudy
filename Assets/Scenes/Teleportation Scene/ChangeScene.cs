c2c9064a1bc5f6e5a8aac62beee1a113297e38ce
elikemk <101428026+elikemk@users.noreply.github.com>
2023-12-07 17:21:42 -0500

    Scene 1

--- Assets/Scenes/Teleportation Scene/ChangeScene.cs
+++ Assets/Scenes/Teleportation Scene/ChangeScene.cs
@@ -1,19 +1,62 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.XR.Interaction.Toolkit;
 
-public class ChangeScene : MonoBehaviour
+namespace UnityEngine.XR.Interaction.Toolkit
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ChangeScene : MonoBehaviour
     {
-        
-    }
+        public XRController xrController;
+        public Canvas vrCanvas;
+        public Button continiousButton;
+        public Button teleportationButton;
 
-    // Update is called once per frame
-    public void ScenceChange()
-    {
-        SceneManager.LoadScene("Teleporation Scene");
+        private bool isTeleporting = false;
+
+        private void Start()
+        {
+            vrCanvas.enabled = true;
+
+            continiousButton.onClick.AddListener(OnContinuousButtonClik);
+            teleportationButton.onClick.AddListener(OnTeleportationButtonClick);
+        }
+
+        private void Update()
+        {
+            float pressThreshold = 0.1f;
+            if (isTeleporting && !IsButtonPressed(xrController.inputDevice, xrController.selectUsage, pressThreshold))
+            {
+                vrCanvas.enabled = true; // Re-enable the canvas after teleportation
+                isTeleporting = false;
+            }
+        }
+
+        private bool IsButtonPressed(InputDevice inputDevice, InputHelpers.Button button, float threshold)
+        {
+            bool isPressed;
+            InputHelpers.IsPressed(inputDevice, button, out isPressed, threshold);
+            return isPressed;
+        }
+
+        public void OnContinuousButtonClik()
+        {
+            vrCanvas.enabled = false;
+        }
+
+        public void OnTeleportationButtonClick()
+        {
+            StartCoroutine(TeleportationCorutine());
+        }
+
+        private IEnumerator TeleportationCorutine()
+        {
+            SceneManager.LoadScene("TeleportationScene");
+
+            isTeleporting = true;
+
+            yield return null;
+        }
     }
 }
